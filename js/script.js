d73ab5c074df770ea3d1ca4ea67400a1505f0652
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-26 16:58:21 +0100

    wip: visualizzazione data invio messaggio e ultimo messaggio con data

--- js/script.js
+++ js/script.js
@@ -14,17 +14,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '01/10/2020 15:30:55',
                             message: 'Hai portato a spasso il cane?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '01/10/2020 15:50:00',
                             message: 'Ricordati di stendere i panni',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 16:15:22',
+                            date: '01/10/2020 16:15:22',
                             message: 'Tutto fatto!',
                             status: 'received'
                         }
@@ -36,17 +36,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '20/03/2020 16:30:00',
+                            date: '03/20/2020 16:30:00',
                             message: 'Ciao come stai?',
                             status: 'sent'
                         },
                         {
-                            date: '20/03/2020 16:30:55',
+                            date: '03/20/2020 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received'
                         },
                         {
-                            date: '20/03/2020 16:35:00',
+                            date: '03/20/2020 16:35:00',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                             status: 'sent'
                         }
@@ -58,17 +58,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '28/03/2020 10:10:40',
+                            date: '03/28/2020 10:10:40',
                             message: 'La Marianna va in campagna',
                             status: 'received'
                         },
                         {
-                            date: '28/03/2020 10:20:10',
+                            date: '03/28/2020 10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
                             status: 'sent'
                         },
                         {
-                            date: '28/03/2020 16:15:22',
+                            date: '03/28/2020 16:15:22',
                             message: 'Ah scusa!',
                             status: 'received'
                         }
@@ -80,12 +80,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '01/10/2020 15:30:55',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '01/10/2020 15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
                             status: 'received'
                         }
@@ -97,12 +97,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '01/10/2020 15:30:55',
                             message: 'Ricordati di chiamare la nonna',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '01/10/2020 15:50:00',
                             message: 'Va bene, stasera la sento',
                             status: 'received'
                         }
@@ -114,17 +114,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '01/10/2020 15:30:55',
                             message: 'Ciao Claudia, hai novità?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '01/10/2020 15:50:00',
                             message: 'Non ancora',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '01/10/2020 15:51:00',
                             message: 'Nessuna nuova, buona nuova',
                             status: 'sent'
                         }
@@ -136,12 +136,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '01/10/2020 15:30:55',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '01/10/2020 15:50:00',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
                             status: 'received'
                         }
@@ -153,17 +153,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '01/10/2020 15:30:55',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '01/10/2020 15:50:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '01/10/2020 15:51:00',
                             message: 'OK!!',
                             status: 'received'
                         }
@@ -190,6 +190,7 @@ createApp({
             // ricerca contatti side bar
             searchQuery: '',
 
+
         }
     },
 
@@ -211,6 +212,7 @@ createApp({
                 message = {
                     message: this.newMessageText,
                     status: 'sent',
+                    date: new Date(),
                 }
                 this.activeContact.messages.push(message);
                 // setto il timeout per la risposta
@@ -219,6 +221,7 @@ createApp({
                     this.newAnswer = {
                         message: this.automaticAnswers[Math.floor(Math.random() * this.automaticAnswers.length)],
                         status: 'received',
+                        date: new Date(),
                     }
 
                     this.activeContact.messages.push(this.newAnswer)
@@ -233,15 +236,18 @@ createApp({
             this.automaticAnswer();
         },
 
-
-
+        // creo una funzione per prelevare l'ora e i minuti di invio del messaggio
         getTime(time) {
+            // creo una variabile con la data del messaggio
             const currentDate = new Date(time);
+            // prelevo data e ora
             const hours = String(currentDate.getHours()).padStart(2, '0');
             const minutes = String(currentDate.getMinutes()).padStart(2, '0');
       
             return `${hours}:${minutes}`;
-          },
+        },
+
+        
         
     },
 
